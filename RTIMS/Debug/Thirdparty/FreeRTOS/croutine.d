d7e57093b1bac4e0459ce61118c48b1030b48510
dharsi100 <you@example.com>
2025-07-03 10:30:38 +0530

    Initial commit

---
+++ RTIMS/Debug/Thirdparty/FreeRTOS/croutine.d
@@ -0,0 +1,21 @@
+Thirdparty/FreeRTOS/croutine.o: ../Thirdparty/FreeRTOS/croutine.c \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/projdefs.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/portable.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/list.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/croutine.h
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/projdefs.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/portable.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/list.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/croutine.h:
---
+++ RTIMS/Debug/Thirdparty/FreeRTOS/timers.d
@@ -0,0 +1,25 @@
+Thirdparty/FreeRTOS/timers.o: ../Thirdparty/FreeRTOS/timers.c \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/projdefs.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/portable.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/list.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/queue.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h \
+ C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/timers.h
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/projdefs.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/portable.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/list.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/queue.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/task.h:
+C:/Users/WNU3KOR/Desktop/RTOS_Workspace/RTIMS/Thirdparty/FreeRTOS/include/timers.h:
