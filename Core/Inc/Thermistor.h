45c2b8b3447736ee95728ad568e12a4468d36fc8
dharsi100 <fixed-term.S.Dharsika@bosch.com>
2025-09-26 16:00:00 +0530

    Update Thermistor.h

--- Core/Inc/Thermistor.h
+++ Core/Inc/Thermistor.h
@@ -3,74 +3,71 @@
 
 #include "stm32f4xx.h"
 #include "cmsis_os.h"
-#include "usart.h"
+#include <stdio.h>
 
+// ===== Globals =====
+float g_temperature = 0.0f;
+
+// ===== Power GPIO (if used to power sensor) =====
 void GPIO_Init_Power(void)
 {
-	RCC->AHB1ENR |= 1<<0; // Enable clock to GPIOA
+    RCC->AHB1ENR |= (1<<0); // Enable GPIOA
 
-    GPIOA->MODER &= ~((3 << (2 * 3)) | (3 << (2 * 4))); // Clear mode PA 3, PA 4
-    GPIOA->MODER |=  ((1 << (2 * 3)) | (1 << (2 * 4))); // Set output mode
+    GPIOA->MODER &= ~((3 << (2*3)) | (3 << (2*4))); 
+    GPIOA->MODER |=  ((1 << (2*3)) | (1 << (2*4))); // PA3, PA4 as output
 
-    GPIOA->ODR |= (1<<3);
+    GPIOA->ODR |= (1<<3); // turn on PA3 (power pin if required)
 }
 
-// ===== ADC1: Thermistor (PA6, Channel 6) =====
-void ADC1_Init_Thermistor()
+// ===== ADC1: Thermistor on PA6 (Channel 6) =====
+void ADC1_Init_Thermistor(void)
 {
-    char str[20];
-    float Temp;
-    uint32_t result;
-
-    // Enable Clocks
+    // Enable clocks
     RCC->AHB1ENR |= (1<<0);    // GPIOA
     RCC->APB2ENR |= (1<<8);    // ADC1
 
     // Configure PA6 as Analog
     GPIOA->MODER |= (3 << (2*6));
 
-    // ADC1 Configuration
-    ADC1->CR1 &= ~(3 << 24);   // 12-bit resolution
-    ADC1->CR2 |= (1<<1);       // Continuous conversion
+    // ADC1 config
+    ADC1->CR1 &= ~(3 << 24);   // 12-bit
+    ADC1->CR2 |= (1<<1);       // Continuous mode
     ADC1->CR2 |= (1<<0);       // ADC ON
 
-    ADC1->SQR1 &= ~(0xF << 20); // 1 conversion
+    ADC1->SQR1 &= ~(0xF << 20); 
     ADC1->SQR3 &= ~(0x1F << 0);
     ADC1->SQR3 |= 6;           // Channel 6
 
     ADC1->CR2 |= (1<<30);      // Start conversion
-
-
-        while (!(ADC1->SR & (1<<1))); // Wait EOC
-        result = ADC1->DR;
-
-        Temp = 125 - (result / 22.7) - 66; // Thermistor formula
-        sprintf(str, "T: %.2f ", Temp);
-        lprint(0x80, str);
-       
-
-        for(int i=0;i<80000;i++); // Delay
-
 }
 
-float Thermistor_ReadTemperature(void) {
-    // Replace with your ADC read + conversion
-    return 25.0f; 
+// ===== Single Read + Convert to Temperature =====
+float Thermistor_ReadTemperature(void)
+{
+    uint32_t result;
+    float Temp;
+
+    while (!(ADC1->SR & (1<<1))); // wait for EOC
+    result = ADC1->DR;
+
+    // Example formula (adjust to your thermistor)
+    Temp = 125.0f - (result / 22.7f) - 66.0f;
+    return Temp;
 }
 
 // ===== RTOS Task =====
-float g_temperature = 0.0f;
-
-void vTaskThermistor(void *pvParameters) {
+void vTaskThermistor(void *pvParameters)
+{
     (void) pvParameters;
-    for (;;) {
+    ADC1_Init_Thermistor();
+
+    for (;;)
+    {
         g_temperature = Thermistor_ReadTemperature();
-        // TODO: send g_temperature to queue or UART
-        vTaskDelay(pdMS_TO_TICKS(1000)); // 1s delay
+
+        // TODO: send g_temperature to LCD / UART / queue
+        vTaskDelay(pdMS_TO_TICKS(1000)); // sample every 1s
     }
 }
 
 #endif
-
-
-
